fa8d2f97ba63e00aa215f00bdebead2de02e6fc1
meghanavaitla <meghanavaitla2000@gmail.com>
2019-12-22 17:38:24 +0430

    commiting alert

--- frontend/js/site.js
+++ frontend/js/site.js
@@ -1,7 +1,7 @@
 //JQUERY makes our life easy
 //1. how to make backend call
 $.getJSON('/data',function(d){
-//alert(JSON.stringify(d));
+alert(JSON.stringify(d));
 $('#name').text(d.name);
 $('#college').text(d.college);
 $('#regno').text(d.regno);
